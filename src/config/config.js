bac73a4a10411429b23c49c24363fd6ad7d5b8eb
Jesse Bowden <jesse@gemr.com>
2016-11-27 00:45:40 -0500

    Fix db:=

--- src/config/config.js
+++ src/config/config.js
@@ -3,26 +3,22 @@ import Sequelize from 'sequelize';
 
 const env = process.env.NODE_ENV;
 
-var sequelizeInstance;
+const sequelizeInstance = (
+    !env || env === 'development' ? (() => (
+        getDatabaseInstance('localhost', 'root', '')
+    ))() : env === "production" ? (() =>
+        getDatabaseInstance('icanhelpyouwiththat', 'root', 'medco123')
+    )() : console.log("Unrecognized enviroment.")
+);
 
 
-export default () => (
-    !env || env === 'development' ? (() => ({
-        db: getDatabaseInstance('localhost', 'root', '')
-    }))() : env === "production" ? (() => {
-        db: getDatabaseInstance('icanhelpyouwiththat', 'root', 'medco123')
-    })() : console.log("Unrecognized enviroment.")
-)
 
 export {sequelizeInstance as sequelize, Sequelize as Sequelize};
 
 function getDatabaseInstance (host, username, password) {
-    return !!sequelizeInstance ? sequelizeInstance : (() => {
-        console.log("Initializing ichuwt mysql...");
-        return sequelizeInstance = new Sequelize('ichuwt', username, password, {
-            host: host,
-            dialect: 'mysql'
-        });
-    })()
+    return new Sequelize('ichuwt', username, password, {
+        host: host,
+        dialect: 'mysql'
+    });
 }
 
